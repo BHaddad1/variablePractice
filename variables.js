2e1abba8157ad893b71762cc4cca708a80f63444
Blanche Haddad <blancherachel@gmail.com>
2022-09-20 15:49:27 -0600

    Save Booleans

--- variables.js
+++ variables.js
@@ -11,7 +11,11 @@ var heightInInches = 61;
 var email = "blancherachel@gmail.com";
 var cat = "Chesty"
 var catAge = 4
+
+// Boolean variables
 var depositPaid = true
+var isEmployed = false
+var likesSodaWater = true
 
 console.log(cat_age);
 console.log(cat);
