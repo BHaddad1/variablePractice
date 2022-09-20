811334f8ea47c8b72129d44f59dccb248d060c72
Blanche Haddad <blancherachel@gmail.com>
2022-09-20 15:27:29 -0600

    Save new variables

--- variables.js
+++ variables.js
@@ -1,6 +1,17 @@
 var name = "Blanche Haddad"
+var name = "Chesty Haddad"
+
+console.log(name)
+
+
 var age = 30
+
 var email = "blancherachel@gmail.com"
 var cat = "Chesty"
-var cat_age = 4
-deposit_paid = true
+var catAge = 4
+var depositPaid = true
+
+console.log(cat_age);
+console.log(cat);
+console.log(catAge);
+console.log(depositPaid);
