6c822c799eb5062d0009cbcb25b1be278574e9e1
Blanche Haddad <blancherachel@gmail.com>
2022-09-20 15:46:32 -0600

    Save semi-colons

--- variables.js
+++ variables.js
@@ -1,12 +1,12 @@
-var name = "Blanche Haddad"
-var name = "Chesty Haddad"
+var name = "Blanche Haddad";
+var name = "Chesty Haddad";
 
 console.log(name)
 
 
 var age = 30
 
-var email = "blancherachel@gmail.com"
+var email = "blancherachel@gmail.com";
 var cat = "Chesty"
 var catAge = 4
 var depositPaid = true
